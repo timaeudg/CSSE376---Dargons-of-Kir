4491ff618c8ef5dcd7c2213749713d0075c29013
Devon Timaeus <timaeudg@rose-hulman.edu>
2013-03-20 14:38:15 -0400

    Added more classes and pretty much finished with adding things, just need to actually start implementing code to allow it to do things

---
+++ Dargons of Kir/Dargons of Kir/DragonTile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dargons_of_Kir
+{
+    class DragonTile
+    {
+    }
+}
---
+++ Dargons of Kir/Dargons of Kir/ProximityTile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dargons_of_Kir
+{
+    class ProximityTile : Tile
+    {
+        public System.Drawing.Image getPicture()
+        {
+            return base.getPicture();
+        }
+    }
+}
