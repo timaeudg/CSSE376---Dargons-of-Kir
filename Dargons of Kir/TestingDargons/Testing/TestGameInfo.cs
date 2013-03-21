0edd28fcb36ee9f6bee7fc1e1ce5db5338892e28
Devon Timaeus <timaeudg@rose-hulman.edu>
2013-03-21 13:25:28 -0400

    Added Testing Framework things, so we can begin TDD

---
+++ Dargons of Kir/TestingDargons/Testing/TestGameInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dargons_of_Kir.Testing
+{
+    [TestFixture]
+    class TestGameInfo
+    {
+
+    }
+    /*
+    [TestFixture]
+    class TestPlayer
+    {
+        [Test]
+        public void playersAreCreatable()
+        {
+            Player player = new Player();
+            Assert.NotNull(player);
+        }
+        [Test]
+        public void playersAreThemSelves()
+        {
+            Player player1 = new Player();
+            Player player2 = new Player();
+            Assert.AreEqual(player1, player1);
+            Assert.AreEqual(player2, player2);
+            Assert.AreNotEqual(player1, player2);
+        }
+        [Test]
+        public void playerDrawTileDoesNotError()
+        {
+            Player player = new Player();
+            player.drawTile();
+        }
+        [Test]
+        public void playerTakeTurns()
+        {
+            Player player = new Player();
+            try
+            {
+                player.takeTurn();
+            }
+            catch (NotImplementedException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+    }*/
+}
