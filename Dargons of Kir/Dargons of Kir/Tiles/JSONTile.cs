c552bb37ea23de75d70d45607a6cef23a60c51b2
Devon Timaeus <timaeudg@rose-hulman.edu>
2013-05-09 16:00:46 -0400

    added some basic functionality to JSONTiles have them to where we can actually make them and they have information about themselves

--- Dargons of Kir/Dargons of Kir/Tiles/JSONTile.cs
+++ Dargons of Kir/Dargons of Kir/Tiles/JSONTile.cs
@@ -4,20 +4,26 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Dargons_of_Kir.Tiles
 {
     public class JSONTile : Tile
     {
-        private static Image getPic()
-        {
-            return Image.FromFile("..\\..\\..\\..\\images\\storm.JPG");
-        }
+        private JSON_File_Info tileInfo;
+        public string name { get { return tileInfo.name; } }
 
-        public JSONTile()
-            : base(getPic())
+        public JSONTile(string filename)
         {
+            StreamReader read = new StreamReader("..\\..\\..\\..\\JSON Tiles\\" + filename);
+            String file = read.ReadToEnd();
 
+            this.tileInfo = JsonConvert.DeserializeObject<JSON_File_Info>(file);
+            this.Priority = this.tileInfo.priority;
+            this.TilePicture = Image.FromFile("..\\..\\..\\..\\images\\"+this.tileInfo.image);
+                 
         }
 
 
@@ -29,6 +35,27 @@ namespace Dargons_of_Kir.Tiles
         public override void placeEffects(Board board)
         {
             
+        }
+
+
+        private sealed class JSON_Effect
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public string inDir { get; set; }
+            public string outDir { get; set; }
+            public int outX { get; set; }
+            public int outY { get; set; }
+        }
+
+        private sealed class JSON_File_Info
+        {
+            public string name { get; set; }
+            public string image { get; set; }
+            public int priority { get; set; }
+            public JSON_Effect[] effects { get; set; }
+
+
         }
 
 
--- Dargons of Kir/TestingDargons/Testing/TestJSON.cs
+++ Dargons of Kir/TestingDargons/Testing/TestJSON.cs
@@ -8,6 +8,8 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using System.Web;
+using Dargons_of_Kir;
+using Dargons_of_Kir.Tiles;
 
 namespace TestingDargons.Testing
 {
@@ -20,19 +22,58 @@ namespace TestingDargons.Testing
         {
             StreamReader re = new StreamReader("..\\..\\..\\..\\JSON Tiles\\TestTile.json");
             String file = re.ReadToEnd();
+
             JObject jsonStuff = JsonConvert.DeserializeObject<JObject>(file);
             JObject testObj = JObject.Parse(file);
+
+
             object priority = testObj["priority"];
-            dynamic data = JsonConvert.DeserializeObject<dynamic>(file);
             int parsedPriority = Convert.ToInt32(priority);
             string parsedName = Convert.ToString(testObj["name"]);
             string filename = Convert.ToString(testObj["image"]);
+
             JToken effects = testObj["Effects"];
             List<JToken> effectList = effects.ToList();
+            JToken firstElement = effectList[0];
+            JObject firstObject = firstElement.ToObject<JObject>();
+            //List<JObject> firstEffect = effectList.Cast<JObject>();
+
             Assert.AreEqual(1, parsedPriority);
             Assert.AreEqual("JSONTest.jpg", filename);
             Assert.AreEqual("TestTile", parsedName);
+
             Assert.NotNull(jsonStuff);
+
+        }
+
+        [Test]
+        public static void TestAnonJSON()
+        {
+            StreamReader re = new StreamReader("..\\..\\..\\..\\JSON Tiles\\TestTile.json");
+            String file = re.ReadToEnd();
+
+            var anonEffect = new {x = new int(), y = new int() , inDir = "" , outDir = "", outX = new int(), outY = new int()};
+            var JSON_FORMAT = new { name = "",
+                                    image = "",
+                                    priority = new int(),
+                                    Effects = new[] {anonEffect} 
+                                  };
+
+            var TestTile = JsonConvert.DeserializeAnonymousType(file, JSON_FORMAT);
+
+            Assert.NotNull(TestTile);
+            Assert.AreEqual("DOWN", TestTile.Effects[0].inDir);
+
+        }
+
+        [Test]
+        public static void TestJSONTileInit()
+        {
+            JSONTile jsonTest = new JSONTile("TestTile.json");
+
+            Assert.NotNull(jsonTest);
+            Assert.AreEqual("TestTile", jsonTest.name);
+
 
         }
         
